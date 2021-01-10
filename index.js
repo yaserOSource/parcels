c9cf9df330eabd18add866e61aabf54729b731b2
Avaer Kazmer <a@modules.io>
2021-01-10 04:04:03 -0500

    Major parcels load debugging

--- index.js
+++ index.js
@@ -10,24 +10,11 @@ const storageHost = `https://ipfs.exokit.org`;
   const parcelsJson = await res.json();
   for (const parcel of parcelsJson) {
     (async () => {
-      const {name, rarity, extents} = parcel;
-      const o = {
-        start_url: `https://webaverse.github.io/parcels/parcel.json`,
-        room: name.replace(/ /g, '-'),
-        extents,
-        rarity,
-      };
-      const s = JSON.stringify(o);
-      const b = new Blob([s], {
-        type: 'application/json',
-      });
-      const u = URL.createObjectURL(b) + '/parcel.url';
-      const object = await world.addStaticObject(u, null, new THREE.Vector3(), new THREE.Quaternion());
-      const contentId = await (async () => {
-        const res = await fetch(`${landHost}/${parcel.id}`);
-        const land = await res.json();
-        const {hash: contentId} = land.properties;
-
+      const res = await fetch(`${landHost}/${parcel.id}`);
+      const land = await res.json();
+      const {hash: contentId} = land.properties;
+      
+      const id = (() => {
         const id = parseInt(contentId, 10);
         if (id > 0) {
           return id;
@@ -45,14 +32,29 @@ const storageHost = `https://ipfs.exokit.org`;
           }
         }
       })();
-      if (contentId) {
+
+      const {name, rarity, extents} = parcel;
+      const o = {
+        contentId: id || `https://webaverse.github.io/parcels/parcel.json`,
+        room: name.replace(/ /g, '-'),
+        extents,
+        rarity,
+      };
+      const s = JSON.stringify(o);
+      const b = new Blob([s], {
+        type: 'application/json',
+      });
+      const u = URL.createObjectURL(b) + '/parcel.url';
+      const object = await world.addStaticObject(u, null, new THREE.Vector3(), new THREE.Quaternion());
+      
+      if (id !== null) {
         const box = new THREE.Box3(
           new THREE.Vector3().fromArray(object.json.extents[0]),
           new THREE.Vector3().fromArray(object.json.extents[1]),
         );
         const center = box.getCenter(new THREE.Vector3());
         center.y = 0;
-        const object2 = world.addStaticObject(contentId, null, center, new THREE.Quaternion());
+        const object2 = world.addStaticObject(id, null, center, new THREE.Quaternion());
       }
     })().catch(console.warn);
   }
