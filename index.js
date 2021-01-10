20cf107958ab4a47f0d17c0fe60706b2d5a6c776
Avaer Kazmer <a@modules.io>
2021-01-09 23:09:22 -0500

    Fetch tokens from chain

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 import * as THREE from 'three';
-import {renderer, camera, runtime, world, universe, physics, ui, rig, app, appManager, popovers} from 'app';
+import {renderer, camera, runtime, world, universe, physics, ui, rig, app, appManager, popovers, crypto} from 'app';
 
 (async () => {
   const res = await fetch(`https://webaverse.github.io/parcels/parcels.json`);
   const parcelsJson = await res.json();
   for (const parcel of parcelsJson) {
-    const {name, rarity, extents} = parcel;
-    const o = {
-      start_url: `https://webaverse.github.io/parcels/parcel.json`,
-      room: name.replace(/ /g, '-'),
-      extents,
-      rarity,
-    };
-    const s = JSON.stringify(o);
-    const b = new Blob([s], {
-      type: 'application/json',
-    });
-    const u = URL.createObjectURL(b) + '/parcel.url';
-    world.addStaticObject(u, null, new THREE.Vector3(), new THREE.Quaternion())
-      .catch(console.warn);
+    (async () => {
+      const {name, rarity, extents} = parcel;
+      const o = {
+        start_url: `https://webaverse.github.io/parcels/parcel.json`,
+        room: name.replace(/ /g, '-'),
+        extents,
+        rarity,
+      };
+      const s = JSON.stringify(o);
+      const b = new Blob([s], {
+        type: 'application/json',
+      });
+      const u = URL.createObjectURL(b) + '/parcel.url';
+      const [object, parcelJson] = await Promise.all([
+        world.addStaticObject(u, null, new THREE.Vector3(), new THREE.Quaternion()),
+        crypto.getParcel(parcel.id),
+      ]);
+      console.log('got parcel json', parcelJson);
+    })().catch(console.warn);
   }
 })();
