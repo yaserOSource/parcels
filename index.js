2ddd644256f2a3a0b49f8c38ae2d14387709445e
Avaer Kazmer <a@modules.io>
2021-01-17 08:33:50 -0500

    Debug empty parcel load

--- index.js
+++ index.js
@@ -91,11 +91,11 @@ const landHost = `https://land.webaverse.com`;
         type: 'application/json',
       });
       const parcelUrl = URL.createObjectURL(b) + '/parcel.url';
-      const bakeUrl = `https://bake.exokit.org/model.glb?u=${u}&e=${JSON.stringify([centerBox.min.toArray(), centerBox.max.toArray()])}`;
+      const bakeUrl = u && `https://bake.exokit.org/model.glb?u=${u}&e=${JSON.stringify([centerBox.min.toArray(), centerBox.max.toArray()])}`;
 
       await Promise.all([
-        await world.addStaticObject(parcelUrl, null, new THREE.Vector3(), new THREE.Quaternion()),
-        await world.addStaticObject(bakeUrl, null, new THREE.Vector3(center.x, box.min.y, center.z), new THREE.Quaternion()),
+        world.addStaticObject(parcelUrl, null, new THREE.Vector3(), new THREE.Quaternion()),
+        bakeUrl ? world.addStaticObject(bakeUrl, null, new THREE.Vector3(center.x, box.min.y, center.z), new THREE.Quaternion()) : Promise.resolve(),
       ]);
     })();
   }
